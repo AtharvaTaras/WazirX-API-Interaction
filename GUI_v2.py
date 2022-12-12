a572b67e1ced97615349f8089fbe1d98940df0a5
AtharvaTaras <atharvataras26@gmail.com>
2022-12-12 21:21:30 +0530

    auto refresh gui

--- GUI_v2.py
+++ GUI_v2.py
@@ -1,8 +1,22 @@
 import cv2
 import numpy as np
 import requests
+from time import sleep
 
 URL = "https://api.wazirx.com/api/v2/tickers"
+PAIR = 'btcinr'
+WAITING_TIME = 1
+
+
+def get_data():
+    response = requests.get(URL)
+    _ = response.json()
+
+    buy = (_[PAIR]['buy'])
+    sell = (_[PAIR]['sell'])
+
+    return [buy, sell]
+
 
 def add_text(text, location, color=(255, 255, 255), scale=1):
 
@@ -16,14 +30,15 @@ def add_text(text, location, color=(255, 255, 255), scale=1):
                 color=color)
 
 
-img = np.zeros((300, 700, 3), dtype=np.uint8)
+while True:
+    data = get_data()
 
+    img = np.zeros((300, 700, 3), dtype=np.uint8)
+    add_text(PAIR, (10, 100), scale=3)
+    add_text(f'Buy Price = {data[0]}', (10, 150))
+    add_text(f'Sell Price = {data[1]}', (10, 200))
 
-add_text('CRYPTO NAME', (10, 100), scale=3)
-add_text('Buy Price = ', (10, 150))
-add_text('Sell Price =', (10, 200))
-
-
-cv2.imshow('API', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow(PAIR.upper(), img)
+    cv2.waitKey(1)
+    sleep(WAITING_TIME)
+    cv2.destroyAllWindows()
